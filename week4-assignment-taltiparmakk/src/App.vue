100342902c9881534cba8ed5d2fe805a64c92a57
Tayfun Altıparmak <91414101+taltiparmakk@users.noreply.github.com>
2022-03-02 22:15:48 +0300

    4.project added

---
+++ week4-assignment-taltiparmakk/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <sideBar/>
+
+  <pageWrapper/>
+</template>
+
+<script>
+
+import sideBar from '@/components/sideBar.vue'
+import pageWrapper from '@/components/pageWrapper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    sideBar,
+    pageWrapper
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ week4-assignment-taltiparmakk/src/components/Wrapper/cardBody.vue
@@ -0,0 +1,33 @@
+<template>
+            <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="chart-pie pt-4 pb-2">
+                                        <canvas id="myPieChart"></canvas>
+                                    </div>
+                                    <div class="mt-4 text-center small">
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-primary"></i> Direct
+                                        </span>
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-success"></i> Social
+                                        </span>
+                                        <span class="mr-2">
+                                            <i class="fas fa-circle text-info"></i> Referral
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    </template>
+
+    <script>
+                
+                export default {
+                name: 'cardBody',
+                components: {
+                }
+                }
+                
+                </script>
